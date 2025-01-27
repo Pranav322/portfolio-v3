ba0a20e0dd565e37aecfba43249a880af8cae63e
pranav322 <pranav88096@gmail.com>
2025-01-27 21:50:30 +0530

    Added tailwind

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["assets.aceternity.com"], // Add your image host here
+    domains: ['assets.aceternity.com'], // Add your image host here
   },
 };
 
