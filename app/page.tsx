936fed6666afc8321c32c6c5f795bf45da807820
pranav322 <pranav88096@gmail.com>
2025-01-27 21:49:10 +0530

    Code is pretty haha

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,22 @@
-'use client'
+'use client';
 import { ToggleButton } from './components/ui/ButtonToggle';
 import { useState } from 'react';
 import FloatingDockDemo from './components/Navbar';
-import Terminalcomp from './components/Terminal';
+import Terminalcomp from './components/Terminal/Terminal';
 import { DesktopIcons } from './components/ui/DesktopIcons';
 
 export default function Home() {
-const [isCLI, setIsCLI] = useState(false);
+  const [isCLI, setIsCLI] = useState(false);
 
-  
   return (
     <div className="relative h-screen w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      
+
       {/* Main content area */}
       <div className="relative h-full flex flex-col">
         {!isCLI && <DesktopIcons />}
-        
+
         {/* Toggle button section */}
         <div className="flex-grow flex items-center justify-center">
           <p className="text-4xl sm:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
@@ -28,13 +27,13 @@ const [isCLI, setIsCLI] = useState(false);
         {/* CLI Mode text */}
         {isCLI && (
           <div className="flex-grow flex items-center justify-center px-4 mb-20">
-          <Terminalcomp />
-        </div>
+            <Terminalcomp />
+          </div>
         )}
 
         {/* Floating dock */}
         {!isCLI && (
-          <FloatingDockDemo 
+          <FloatingDockDemo
             desktopClassName="fixed bottom-8 left-1/2 -translate-x-1/2"
             mobileClassName="fixed bottom-8 right-8"
           />
