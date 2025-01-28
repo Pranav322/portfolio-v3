0328f5b6d7cf414b7211484e2b4a3b85ace88a31
pranav322 <pranav88096@gmail.com>
2025-01-28 15:56:12 +0530

    bro keeps changings

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,10 @@ export default function Home() {
     : {};
 
   return (
-    <div className="relative h-screen w-full overflow-hidden" style={backgroundStyle}>
+    <div
+      className="relative h-screen w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2]"
+      style={backgroundStyle}
+    >
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
