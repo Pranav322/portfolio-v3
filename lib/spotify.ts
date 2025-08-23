84c41ba1fc30a232f112a7943b7e5c995e79b5b6
pranav322 <pranav88096@gmail.com>
2025-08-23 22:54:00 +0530

    chhota mota fixes

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,7 +1,6 @@
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
-import { Track } from '@/types/spotify';
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
 const NOW_PLAYING_ENDPOINT = 'https://api.spotify.com/v1/me/player/currently-playing';
